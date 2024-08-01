68a7b69e7e5066483e76fea4036e015f07d6ee55
윤동휘 <xorms5712@gmail.com>
2024-08-01 15:46:39 +0900

    feat: Add create new data api

--- backend/service.js
+++ backend/service.js
@@ -1,5 +1,6 @@
 const pool = require("./db");
 
+
 // 테스트
 async function getTest() {
     const client = await pool.connect();
@@ -33,9 +34,39 @@ async function getAllData() {
     }
 }
 
+// 수입/지출 데이터 입력하기
+async function createData(requestData) {
+    const client = await pool.connect();
+
+    try {
+        const insertQuery = `
+            INSERT INTO account_book (category, is_income, content, amount)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *;
+        `;
+
+        const values = [
+            requestData.getCategory(),
+            requestData.getIsIncome(),
+            requestData.getContent(),
+            requestData.getAmount(),
+        ];
+
+        const result = await client.query(insertQuery, values);
+
+        return result.rows[0];
+    } catch (err) {
+        console.error("Error executing query:", err);
+        throw err;
+    } finally {
+        client.release();
+    }
+}
+
 
 
 module.exports = {
     getTest,
     getAllData,
+    createData,
 };
