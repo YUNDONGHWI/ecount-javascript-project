0f7c71aa5e73da1bcba5586a3e22d798ac4e8a8c
유승부 <yoosb0328@gmail.com>
2024-08-01 12:44:26 +0900

    api test
    router-controller-service-db 동작 확인

--- backend/controller.js
+++ backend/controller.js
@@ -1,12 +1,12 @@
-const service = require('./service');
+const service = require("./service");
 
 async function test(req, res) {
     try {
         const data = await service.getTest();
-        console.log(data);
-    } catch(err) {
+        res.status(200).json(data);
+    } catch (err) {
         console.error(err);
-        res.status(500).send('Error ' + err.message);
+        res.status(500).json({ error: "Internal Server Error " });
     }
 }
 
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const router = require('./app');
-const controller = require('./controller');
+const port = 5000;
+const router = require("./router");
+const controller = require("./controller");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 //CORS 설정 후 라우터 사용 필요.
 
+// /api 경로 요청에 대해 router 사용
+app.use("/api", router);
+
 //서버 실행
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-//테스트 목적으로 컨트롤러 직접 호출
-controller.test();
+    console.log(`Example app listening on port ${port}`);
+});
